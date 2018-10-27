6b36e8870e657f9d8ccf31c39b4713caf577b25a
Dmitriy Dubson <d.dubson@gmail.com>
2018-10-27 14:34:02 -0400

    Add simple logo

--- web/src/AppHeader.tsx
+++ web/src/AppHeader.tsx
@@ -5,6 +5,9 @@ export class AppHeader extends PureComponent {
   render() {
     return (
       <nav className="app-nav">
+          <div className="app-logo">
+            <img src={require("./images/library.svg")} alt="book-store-logo" />
+          </div>
           <div data-test="page-title">
             Book Store
           </div>
