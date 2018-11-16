6e44ea0599aae5117ffdd8cb1e6af2bfdfd2313c
Dmitriy Dubson <ddubson@pivotal.io>
2018-11-16 17:30:07 -0500

    Fixing firebase lib issues

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,25 +1,25 @@
 import * as React from "react";
 import {PureComponent} from "react";
 import {AppBody} from "./AppBody";
-import Book from "./books/Book";
-import {getAllBooks} from "./books/GetAllBooks.service";
 import {AppFooter} from "./AppFooter";
 import AppHeader from "./AppHeader";
+import Book from "./books/Book";
+import {getAllBooks} from "./books/GetAllBooks.service";
 
 export interface AppProps {
-  getAllBooks: () => Promise<Book[]>,
+  getAllBooks: () => Promise<Book[]>;
 }
 
 class App extends PureComponent<AppProps> {
-  render(): JSX.Element {
+  public render(): JSX.Element {
     return (
       <React.Fragment>
         <AppHeader />
         <AppBody getAllBooks={getAllBooks} />
         <AppFooter />
       </React.Fragment>
-    )
-  };
+    );
+  }
 }
 
 export default App;
